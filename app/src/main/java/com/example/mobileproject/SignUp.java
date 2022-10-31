94be29c53056ec73e87bb3336aa1d15168dc3b3c
Abdalla Gaber <48131968+abdallagaber@users.noreply.github.com>
2022-10-31 22:20:26 +0200

    signUp Database

--- app/src/main/java/com/example/mobileproject/SignUp.java
+++ app/src/main/java/com/example/mobileproject/SignUp.java
@@ -1,14 +1,139 @@
 package com.example.mobileproject;
 
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUp extends AppCompatActivity {
+    private EditText editTextFName,editTextLName,editTextEmail,editTextPhone,editTextPass,editTextConfirmPass;
+    private static final String TAG = "SignUp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextFName = findViewById(R.id.editTextRegFname);
+        editTextLName = findViewById(R.id.editTextRegLname);
+        editTextPhone = findViewById(R.id.editTextRegPhone);
+        editTextEmail = findViewById(R.id.editTextRegEmail);
+        editTextPass = findViewById(R.id.editTextRegPass);
+        editTextConfirmPass = findViewById(R.id.editTextRegConfirmPass);
+        Button buttonRegiser = findViewById(R.id.buttonRegSignup);
+        buttonRegiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstName = editTextFName.getText().toString();
+                String lastName = editTextLName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String phone = editTextPhone.getText().toString();
+                String pass = editTextPass.getText().toString();
+                String confirmPass = editTextConfirmPass.getText().toString();
+
+
+                if (TextUtils.isEmpty(firstName)){
+                    editTextFName.setError("First Name is required");
+                    editTextFName.requestFocus();
+                } else if (TextUtils.isEmpty(lastName)){
+                    editTextLName.setError("First Name is required");
+                    editTextLName.requestFocus();
+                } else if (TextUtils.isEmpty(email)){
+                    editTextEmail.setError("Email is required");
+                    editTextEmail.requestFocus();
+                } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    editTextEmail.setError("Valid Email is required");
+                    editTextEmail.requestFocus();
+                } else if (TextUtils.isEmpty(pass)){
+                    editTextPass.setError("Password is required");
+                    editTextPass.requestFocus();
+                } else if (TextUtils.isEmpty(phone)){
+                    editTextPhone.setError("Phone is required");
+                    editTextPhone.requestFocus();
+                } else if (pass.length() < 8){
+                    editTextPass.setError("Password is weak");
+                    editTextPass.requestFocus();
+                } else if (TextUtils.isEmpty(confirmPass)){
+                    editTextConfirmPass.setError("Password Confirmation is required");
+                    editTextConfirmPass.requestFocus();
+                } else if (!pass.equals(confirmPass)){
+                    editTextConfirmPass.setError("Password Confirmation is required");
+                    editTextConfirmPass.requestFocus();
+                } else {
+                    registerUser(firstName,lastName,phone,email,pass);
+                }
+
+
+            }
+        });
+
+
+    }
+
+    private void registerUser(String firstName, String lastName, String phone, String email, String pass) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                    HashMap<String,Object> userDataMap = new HashMap<>();
+                    userDataMap.put("firstName",firstName);
+                    userDataMap.put("lastName",lastName);
+                    userDataMap.put("phone",phone);
+
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+                    reference.child(firebaseUser.getUid()).updateChildren(userDataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            Toast.makeText(SignUp.this, "User registered succesfully", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(SignUp.this, MainActivity.class);
+                            finish();
+
+                        }});
+                }else {
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        editTextPass.setError("Your password is too weak. Kindly use a mix of alphabets, numbers and special characters");
+                        editTextPass.requestFocus();
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextEmail.setError("Your email is invalid or already in use, Kindly re-enter");
+                        editTextEmail.requestFocus();
+                    }catch (FirebaseAuthUserCollisionException e){
+                        editTextEmail.setError("User is already registered with this email, Use another email");
+                        editTextEmail.requestFocus();
+                    }catch (Exception e){
+                        Log.e(TAG,e.getMessage());
+                        Toast.makeText(SignUp.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 }
