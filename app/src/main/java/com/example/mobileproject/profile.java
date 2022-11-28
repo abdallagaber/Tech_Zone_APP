f83f163522689a9cb6c419a095b0960ba1c7396d
3kwa <cds.Aliamr60130@aleu.edu.eg>
2022-11-28 07:47:42 +0200

    sign out button in profile page

--- app/src/main/java/com/example/mobileproject/profile.java
+++ app/src/main/java/com/example/mobileproject/profile.java
@@ -6,11 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profile extends AppCompatActivity {
 BottomNavigationView bottomNavigationView;
+    private FirebaseAuth auth;
+    private Button btnsignout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,5 +59,21 @@ BottomNavigationView bottomNavigationView;
                 return false;
             }
         });
+
+
+        auth = FirebaseAuth.getInstance();
+        btnsignout = findViewById(R.id.signOutButton);
+
+        btnsignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+                Intent intent = new Intent(profile.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(profile.this, "Signed out Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
