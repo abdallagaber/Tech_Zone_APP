ae817c2f9888554733f3d667c85ab6a4889ff963
3kwa <cds.Aliamr60130@aleu.edu.eg>
2022-11-28 19:27:33 +0200

    Home Page

--- app/src/main/java/com/example/mobileproject/HomeActivity.java
+++ app/src/main/java/com/example/mobileproject/HomeActivity.java
@@ -2,15 +2,20 @@ package com.example.mobileproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mobileproject.ViewHolder.ProductViewHolder;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,9 +24,20 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.firebase.ui.database.*;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity {
 BottomNavigationView bottomNavigationView;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference productRef;
+    private RecyclerView recyclerview;
+    RecyclerView.LayoutManager layoutmanager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +74,39 @@ BottomNavigationView bottomNavigationView;
             }
         });
 
+        productRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        recyclerview = findViewById(R.id.lap);
+        recyclerview.setHasFixedSize(true);
+        layoutmanager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        recyclerview.setLayoutManager(layoutmanager);
+
+    }
+
+    protected void onStart(){
+        super.onStart();
+
+        FirebaseRecyclerOptions<product> options =
+                new FirebaseRecyclerOptions.Builder<product>().setQuery(productRef,product.class).build();
+
+        FirebaseRecyclerAdapter<product, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<product, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull product model) {
+                holder.txtProductName.setText(model.getName());
+                holder.txtProductPrice.setText("EGP "+model.getPrice());
+                Picasso.get().load(model.getImage()).into(holder.productImage);
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items,parent,false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+        recyclerview.setAdapter(adapter);
+        adapter.startListening();
 
     }
 }
---
+++ app/src/main/java/com/example/mobileproject/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,39 @@
+package com.example.mobileproject.ViewHolder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.mobileproject.Interface.ItemClickListner;
+import com.example.mobileproject.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtProductName,txtProductPrice;
+    public ImageView productImage;
+    public ItemClickListner listner;
+
+    public ProductViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+
+        productImage = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_price);
+
+}
+    public void setItemClickLitner(ItemClickListner listner){
+        this.listner = listner;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        listner.onClick(v, getAdapterPosition(),false);
+    }
+}
