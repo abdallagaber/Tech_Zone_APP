a73f42fa845004e5f7b21731995d59100aebac51
rawanshawky <rawanshawky15@gmail.com>
2022-12-18 17:17:58 +0200

    Upload Image

---
+++ app/src/main/java/com/example/mobileproject/Image.java
@@ -0,0 +1,22 @@
+package com.example.mobileproject;
+
+public class Image {
+
+    public Image(){
+
+    }
+
+    String image;
+
+    public Image(String image){
+        this.image = image;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/mobileproject/UploadPic.java
+++ app/src/main/java/com/example/mobileproject/UploadPic.java
@@ -1,14 +1,121 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.mobileproject.databinding.ActivityUploadPicBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class UploadPic extends AppCompatActivity {
+
+    ActivityUploadPicBinding binding;
+    Uri uri;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseStorage firebaseStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_pic);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        binding = ActivityUploadPicBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.chooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UploadImage();
+            }
+        });
+
+        binding.upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final StorageReference reference = firebaseStorage.getReference().child(" Upload Image ").child("images").child(System.currentTimeMillis()+"");
+                reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Image image = new Image();
+                                image.setImage(uri.toString());
+
+                                firebaseDatabase.getReference().child("data").push().setValue(image).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(UploadPic.this, " Upload Successfully", Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent(UploadPic.this, profile.class);
+                                        startActivity(i);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(UploadPic.this, "Upload Error", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                            }
+
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+
+    private void UploadImage() {
+
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(i, 101);
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(UploadPic.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101 && resultCode == RESULT_OK){
+            uri = data.getData();
+            binding.profView.setImageURI(uri);
+        }
     }
 }
